c92b216cd45aa64ec3a34b15aeb95ebf6e737aa6
Prateek Madapurmath <prateekm33@gmail.com>
2016-10-02 21:20:27 -0700

    (cleanup) moves around file structure

--- servers/auth/auth_server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const authServer = express();
-
-authServer.use(express.static(__dirname + '/../../auth_src'));
-  
-authServer.listen(4000, () => { console.log('AUTHENTICATION SERVICE LISTENING ON PORT 4000')});
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,6 +3,8 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const path = require('path');
 
+console.log("DIRNAME : ", __dirname);
+
 module.exports = webpackMerge(commonConfig, {
   output: {
     path: path.resolve(__dirname, '..'),
